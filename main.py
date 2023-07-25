6211584ded29019a1ce17a43d50cb62d13f56eb8
timfuerth <tim.fuerth@hakneumarkt.at>
2023-07-25 15:05:28 +0200

    encrypting/decrypting a whole file with a pwd works now

--- main.py
+++ main.py
@@ -1,16 +1,29 @@
-# This is a sample Python script.
-
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+from utils.encrypt import decrypt_data
+from utils.encrypt import encrypt_data_and_save
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+def unlock_pwd():
+    password = bytes(input("pwd: "), "UTF-8")
+    # msg = bytes(input("msg: "), "UTF-8")
+
+    decrypted_message = decrypt_data(password)
+    print("Decrypted message:", decrypted_message)
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+def encrypt_pwd():
+    password = bytes(input("pwd: "), "UTF-8")
+    msg = bytes(input("msg: "), "UTF-8")
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    encrypt_data_and_save(password)
+    print("Encrypted")
+
+def start():
+    match input("e/d: "):
+        case "e":
+            encrypt_pwd()
+        case "d":
+            unlock_pwd()
+    start()
+
+
+start()
--- utils/encrypt.py
+++ utils/encrypt.py
@@ -1,10 +1,33 @@
 from cryptography.fernet import Fernet
 import base64
+import os
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
+import zipfile
 
-def encrypt_data_and_save(password, plaintext_message):
+
+def derive_key_from_password(password, salt):
+    # Generate a key from the password and salt using PBKDF2
+    kdf = PBKDF2HMAC(
+        algorithm=hashes.SHA256(),
+        length=32,
+        salt=salt,
+        iterations=100000,  # Use the same number of iterations as in encryption
+        backend=default_backend()
+    )
+    key = base64.urlsafe_b64encode(kdf.derive(password))
+
+    return key
+
+
+def encrypt_data_and_save(password):
+    with open('encrypted_data.bin', 'rb') as file:
+        plaintext_message = file.read()
     # Generate a Fernet key
-    key = Fernet.generate_key()
-
+    salt = os.urandom(16)
+    key = derive_key_from_password(password, salt)
+    print(key)
     # Initialize the Fernet cipher with the generated key
     cipher_suite = Fernet(key)
 
@@ -12,7 +35,31 @@ def encrypt_data_and_save(password, plaintext_message):
     encrypted_message = cipher_suite.encrypt(plaintext_message)
 
     # Save the encrypted data and key to a file
-    with open('encrypted_data.bin', 'wb') as file:
-        file.write(key)
+
+    with open('encrypted_data.bin', 'wb') as file: # SAVE PWD HASH / SALT
+        file.write(base64.urlsafe_b64encode(salt))
+        # file.write(b'|')
         file.write(encrypted_message)
+
+
+def decrypt_data(password):
+    # Read the encrypted data and key from the file
+    with open('encrypted_data.bin', 'rb') as file:
+        encrypted_data_with_key = file.read()
+
+    # Separate key from encrypted data
+    salt = base64.urlsafe_b64decode(encrypted_data_with_key[:24])
+    key = derive_key_from_password(password, salt)
+    print(key)
+    encrypted_data = encrypted_data_with_key[24:]
+
+    # Initialize the Fernet cipher with the key
+    cipher_suite = Fernet(key)
+
+    # Decrypt the data
+    decrypted_data = cipher_suite.decrypt(encrypted_data)
+    with open('encrypted_data.bin', 'wb') as dec_file:
+        dec_file.write(decrypted_data)
+    return decrypted_data
+
 
