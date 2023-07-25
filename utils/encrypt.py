7bfa1c6735840b82e26d24406b3a60b00253f97f
timfuerth <tim.fuerth@hakneumarkt.at>
2023-07-25 16:02:54 +0200

    hiding salt/data in a locked zip

--- utils/encrypt.py
+++ utils/encrypt.py
@@ -4,8 +4,7 @@ import os
 from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
-import zipfile
-
+import pyminizip
 
 def derive_key_from_password(password, salt):
     # Generate a key from the password and salt using PBKDF2
@@ -22,7 +21,7 @@ def derive_key_from_password(password, salt):
 
 
 def encrypt_data_and_save(password):
-    with open('encrypted_data.bin', 'rb') as file:
+    with open('encrypted_data.csv', 'rb') as file:
         plaintext_message = file.read()
     # Generate a Fernet key
     salt = os.urandom(16)
@@ -36,15 +35,19 @@ def encrypt_data_and_save(password):
 
     # Save the encrypted data and key to a file
 
-    with open('encrypted_data.bin', 'wb') as file: # SAVE PWD HASH / SALT
+    with open('encrypted_data.csv', 'wb') as file:  # SAVE PWD HASH / SALT
         file.write(base64.urlsafe_b64encode(salt))
         # file.write(b'|')
         file.write(encrypted_message)
 
+    pyminizip.compress('encrypted_data.csv', None, "data.zip", password, 0)
+    os.remove("encrypted_data.csv")
 
 def decrypt_data(password):
+    pyminizip.uncompress("data.zip", password, None, 0)
+    os.remove("data.zip")
     # Read the encrypted data and key from the file
-    with open('encrypted_data.bin', 'rb') as file:
+    with open('encrypted_data.csv', 'rb') as file:
         encrypted_data_with_key = file.read()
 
     # Separate key from encrypted data
@@ -58,8 +61,6 @@ def decrypt_data(password):
 
     # Decrypt the data
     decrypted_data = cipher_suite.decrypt(encrypted_data)
-    with open('encrypted_data.bin', 'wb') as dec_file:
+    with open('encrypted_data.csv', 'wb') as dec_file:
         dec_file.write(decrypted_data)
     return decrypted_data
-
-
