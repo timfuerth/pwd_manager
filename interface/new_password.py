97c1eb56c467ace37f98b4bb5fe335d6ea1ff8ae
jakobfriedl <jakob@friedl.email>
2023-07-25 16:36:38 +0200

    use ttk instead of tk

--- interface/new_password.py
+++ interface/new_password.py
@@ -1,4 +1,4 @@
-import tkinter as tk
+from tkinter import ttk
 from pwd_manager.interface import window_base as wb
 from pwd_manager.interface import password
 
@@ -7,15 +7,15 @@ class New_password_window(wb.Window):
 
     def __init__(self):
         super().__init__("new Password")
-        self.username = tk.Entry(self.window, width=15)
-        self.password = tk.Entry(self.window, width=15)
-        self.comment = tk.Entry(self.window, width=15)
+        self.username = ttk.Entry(self.window, width=15)
+        self.password = ttk.Entry(self.window, width=15)
+        self.comment = ttk.Entry(self.window, width=15)
 
     def start(self):
         self.username.grid()
         self.password.grid()
         self.comment.grid()
-        tk.Button(self.window, text="create new password", command=self.save_passwd).grid()
+        ttk.Button(self.window, text="create new password", command=self.save_passwd).grid()
 
         super().start()
 
