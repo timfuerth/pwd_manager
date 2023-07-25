87debe0b3af73b057a14f53ef762cbfca88cadb9
jakobfriedl <jakob@friedl.email>
2023-07-25 14:41:05 +0200

    password window

--- interface/passwords.py
+++ interface/passwords.py
@@ -1,6 +1,21 @@
-from pwd_manager.interface import windows_base
+from pwd_manager.interface import window_base as wb
+import tkinter as tk
 
-class Passwords_window:
 
-    pass
+class Passwords_window(wb.Window):
 
+    def __init__(self, passwords: dict):
+        super().__init__(title="Your Passwords")
+        self.passwords = passwords
+
+    def show_passwords(self):
+        row_index = 0
+        for pw in self.passwords:
+            name = tk.Label(self.window, width=15, text=pw)
+            password = tk.Label(self.window, width=15, text=self.passwords[pw])
+
+            name.grid(column=0, row=row_index)
+            password.grid(column=1, row=row_index)
+            row_index += 1
+
+    ### TODO: only show passwords as plaintext when button is pressed
