9381c186128200425fed63d7b63d4ff9cee9a770
jakobfriedl <jakob@friedl.email>
2023-07-25 14:41:36 +0200

    show passwords when master password matches string

--- interface/login.py
+++ interface/login.py
@@ -1,4 +1,5 @@
 from pwd_manager.interface import window_base as wb
+from pwd_manager.interface import passwords as pw
 import tkinter as tk
 from tkinter import ttk
 
@@ -6,11 +7,10 @@ from tkinter import ttk
 class Login_window(wb.Window):
 
     def __init__(self):
-        super().__init__()
-        self.window.title("login")
+        super().__init__(title="login")
         self.password = tk.StringVar()
         self.passwd_entry = ttk.Entry(self.window, show="*", width=15, textvariable=self.password)
-        self.paswd_button = ttk.Button(self.window, text="login", width=4, command=self.login())
+        self.paswd_button = ttk.Button(self.window, text="login", width=4, command=self.login)
 
     def start(self):
         self.passwd_entry.pack()
@@ -18,13 +18,15 @@ class Login_window(wb.Window):
         super().start()
 
     def login(self):
-        print(self.password.get())
+        if self.password.get() != "123":
+            tk.Label(self.window, text=self.password.get()).pack()
+            return
 
+        pw_window = pw.Passwords_window({
+            "pass1": "abc",
+            "pass2": "cde",
+            "pass3": "fgh"
+        })
 
-
-
-
-
-
-
-
+        pw_window.show_passwords()
+        pw_window.start()
