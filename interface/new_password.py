3d1e7239d0aa2d61b591713db37043caf0a80a1e
jakobfriedl <jakob@friedl.email>
2023-07-26 14:02:52 +0200

    start save_passwd

--- interface/new_password.py
+++ interface/new_password.py
@@ -1,15 +1,18 @@
+import os
 from tkinter import ttk
 from pwd_manager.interface import window_base as wb
 from pwd_manager.interface import password
+from pwd_manager.utils.encrypt import encrypt_one_file
 
 
 class New_password_window(wb.Window):
 
-    def __init__(self):
+    def __init__(self, key):
         super().__init__("new Password")
         self.username = ttk.Entry(self.window)
         self.password = ttk.Entry(self.window)
         self.comment = ttk.Entry(self.window)
+        self.key = key
 
     def start(self):
         ttk.Label(self.window, text="Username: ").grid(row=0, column=0)
@@ -22,4 +25,17 @@ class New_password_window(wb.Window):
         super().start()
 
     def save_passwd(self):
-        return password.Password(username=self.username, password=self.password, comment=self.comment)
+        passw_file_id = 0
+        file_name = f"{passw_file_id}.csv"
+
+        while os.path.isfile(file_name):
+            passw_file_id = 0
+            file_name = f"{passw_file_id}.csv"
+
+        with open(file=file_name, mode='x') as file:
+            pw = password.Password(username=self.username, password=self.password, comment=self.comment)
+            print(str(pw))
+            file.write(str(pw))
+
+            encrypt_one_file(self.key, file_name)
+            file.close()
