fea2f95c7e0f02e794bbf32dd8afff26296646fa
jakobfriedl <jakob@friedl.email>
2023-07-26 08:28:11 +0200

    rename variables

--- interface/passwords.py
+++ interface/passwords.py
@@ -1,17 +1,17 @@
 from pwd_manager.interface import window_base as wb
-from pwd_manager.interface import password as pwd
+from pwd_manager.interface import password as pw
 from pwd_manager.interface import new_password
 import tkinter as tk
 
 
 class Row:
-    def __init__(self, window, row_index, pw: pwd.Password):
+    def __init__(self, window, row_index, passw: pw.Password):
         self.row_index = row_index
         self.hidden = True
-        self.name = tk.Label(window, width=15, text=pw.username)
-        self.password = tk.Label(window, width=15, text=pw.password)
+        self.name = tk.Label(window, width=15, text=passw.username)
+        self.password = tk.Label(window, width=15, text=passw.password)
         self.show_password_button = tk.Button(window, width=15, text="show", command=self.show_hide_password)
-        self.comment = tk.Label(window, width=15, text=pw.comment)
+        self.comment = tk.Label(window, width=15, text=passw.comment)
 
         self.name.grid(column=0, row=row_index)
         self.password.grid(column=1, row=row_index)
@@ -34,14 +34,14 @@ def create_new_password():
 
 class Passwords_window(wb.Window):
 
-    def __init__(self, passwords: list[pwd.Password]):
+    def __init__(self, passwords: list[pw.Password]):
         super().__init__(title="Your Passwords")
         self.passwords = passwords
 
     def show_passwords(self):
         row_index = 0
-        for pw in self.passwords:
-            Row(self.window, row_index, pw)
+        for passw in self.passwords:
+            Row(self.window, row_index, passw)
             row_index += 1
 
     def start(self):
