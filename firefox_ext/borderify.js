ba43b0c0f4b8dc35f67db7a7055edcd3b68349c5
jakobfriedl <jakob@friedl.email>
2023-07-26 15:35:40 +0200

    autotype password & email on any website

--- firefox_ext/borderify.js
+++ firefox_ext/borderify.js
@@ -1,1 +1,25 @@
 document.body.style.border = "5px solid red";
+
+let inputs = document.getElementsByTagName("input")
+
+Array.from(inputs).forEach(element => {
+    
+    switch (element.type.toLowerCase()) {
+        case "password":
+            element.value = "123"
+            break;
+        case "email":
+            element.value = "abc@gmail.com"
+
+        default:
+            break;
+    }
+
+});
+
+
+let ptags = document.getElementsByTagName("p")
+
+for(let i = 0; i < ptags.length; i++){
+    ptags[i].style.border = "5px solid red";
+}
