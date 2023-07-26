420a0c8133d26bcf45d8cadf4636913944f7692b
jakobfriedl <jakob@friedl.email>
2023-07-26 09:24:37 +0200

    add functions

--- utils/csv_to_password.py
+++ utils/csv_to_password.py
@@ -1,11 +1,30 @@
 import csv
 from pwd_manager.utils import password as pw
+from pwd_manager.utils.encrypt import decrypt_data, encrypt_line
 
-def read(decrypted_csv: str):
-    for row in csv.reader(csvfile=decrypted_csv, delimiter=';', lineterminator="\n"):
-        username, password, comment = row
 
-        new_password = pw.Password(username, password, comment)
+def get_decrypted_passwords(key):
+
+    decrypted_passwords: list[pw.Password] = []
+
+    with decrypt_data(key) as file:
+        for row in csv.reader(csvfile=file, delimiter=';'):
+            username, password, comment = row
+            password = pw.Password(username, password, comment)
+            decrypted_passwords += password
+
+    return decrypted_passwords
+
+
+def encrypt_password(password, key):
+
+    with open('encrypted_data.csv', 'a') as file:
+
+        row = password.username, password.password, password.comment
+        encrypt_line(row, key)
+
+        writer = csv.writer(csvfile=file, delimiter=';')
+        writer.writerow(row)
 
 
 
