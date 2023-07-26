46e9e2a9650f3987a50a035ee4ca0ae9106f89c5
jakobfriedl <jakob@friedl.email>
2023-07-26 11:42:59 +0200

    add descriptions to input fields

--- interface/new_password.py
+++ interface/new_password.py
@@ -12,11 +12,13 @@ class New_password_window(wb.Window):
         self.comment = ttk.Entry(self.window, width=15)
 
     def start(self):
-        self.username.grid()
-        self.password.grid()
-        self.comment.grid()
+        ttk.Label(self.window, text="Username: ").grid(row=0, column=0)
+        self.username.grid(row=0, column=1)
+        ttk.Label(self.window, text="Password: ").grid(row=1, column=0)
+        self.password.grid(row=1, column=1)
+        ttk.Label(self.window, text="Comment: ").grid(row=2, column=0)
+        self.comment.grid(row=2, column=1)
         ttk.Button(self.window, text="create new password", command=self.save_passwd).grid()
-
         super().start()
 
     def save_passwd(self):
